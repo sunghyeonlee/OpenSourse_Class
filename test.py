83addaf775100133bfb893ce932791dbd184d0db
sunghyeonlee <psook123@naver.com>
2020-10-11 20:41:57 +0900

    Update test.py

--- test.py
+++ test.py
@@ -13,6 +13,6 @@ def findMax(a,b,c):
     b = int(input("두 번째 숫자 입력:"))
     c = int(input("세 번째 숫자 입력:"))
 
-    biggest=findMax(a,b,c)
+    biggest = findMax(a, b, c)
 
     print(a,b,c,"중 가장 큰 수는",biggest,"입니다.")
